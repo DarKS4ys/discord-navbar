3f31add7c5db87e8eac83df274e87798961f3e1d
DarKS4ys <melihyardim1057@gmail.com>
2023-08-06 16:53:44 +0300

    discord navbar

---
+++ app/chat/page.js
@@ -0,0 +1,9 @@
+export default function Chat() {
+  return (
+    <div className="flex ml-16 bg-[var(--backgroundcolor)] text-white">
+      <div>
+        <h1> ADSADASD S</h1>
+      </div>
+    </div>
+  );
+}
---
+++ components/sidebar.js
@@ -0,0 +1,36 @@
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { FaFire, FaPoo } from 'react-icons/fa';
+
+const Sidebar = () => {
+  return (
+    <div className="fixed flex flex-col justify-between items-center top-0 left-0 h-screen w-16 m-0 bg-gray-900 text-secondary shadow-lg">
+      <Link href="/chat">
+        <SidebarIcon icon={<FaFire size="28" />} tooltip="Fire" />
+      </Link>
+      <SidebarIcon
+        icon={<BsPlus size="32" />}
+        tooltip="Plus"
+        color="text-green-500"
+      />
+      <SidebarIcon
+        icon={<BsFillLightningFill size="20" />}
+        tooltip="Lightning"
+      />
+      <SidebarIcon icon={<FaPoo size="20" />} tooltip="Poo" />
+      <div className="mt-auto">
+        <SidebarIcon icon={<BsGearFill size="20" />} tooltip="Settings" />
+      </div>
+    </div>
+  );
+};
+
+const SidebarIcon = ({ icon, tooltip, color }) => (
+  <div className={cn('sidebar-icon group', color)}>
+    {icon}
+    <span className="sidebar-tooltip group-hover:scale-100">{tooltip}</span>
+  </div>
+);
+
+export default Sidebar;
